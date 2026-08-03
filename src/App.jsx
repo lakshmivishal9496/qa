1a7d6f4648b935a5da72cc5c1dca4fb0bca00422
lakshmivishal9496 <lakshmivishal9496@gmail.com>
2026-08-03 10:28:34 +0200

    Added responsive design for media query

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,11 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
-
 import Experience from './pages/Experience';
 import Contributions from './pages/Contributions';
+
 // 3. GLOBAL STYLES
 import './styles/Global.css';
-
 
 function App() {
   return (
@@ -22,11 +21,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
-            <Route path="about" element={<Experience />} />
-             <Route path="about" element={<Contributions />} />
+          <Route path="about" element={<Experience />} />
+          <Route path="contributions" element={<Contributions />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/contributions" element={<Contributions />} />
         </Route>
       </Routes>
     </BrowserRouter>
